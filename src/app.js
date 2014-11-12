b1c17437846c19c32e326f0bb5ae9181575b90e1
Paul Sonnentag <Paul.Sonnentag@googlemail.com>
2014-11-12 15:40:17 +0100

    Adds positional coloring of nodes

--- src/app.js
+++ src/app.js
@@ -8,14 +8,13 @@ var radius = 40;
 var width = 1000;
 var height = 800;
 
+//TODO: remove global vars
 var data = [];
-
-var color = d3.scale.category20();
+var updateColor = true;
 
 var graph = d3.select('#graph')
   .attr('width', width)
   .attr('height', height);
-
 
 var force = d3.layout.force()
   .gravity(0.07)
@@ -23,17 +22,25 @@ var force = d3.layout.force()
   .size([width, height])
   .on('tick', updateNodes);
 
-function updateNodes () {
+function updateNodes() {
   var qTree = d3.geom.quadtree(data);
 
   _.each(data, function (node) {
     qTree.visit(collide(node))
   });
 
+  var nodes = graph.selectAll('g.node')
+    .attr('transform', function (d) {
+      return 'translate(' + d.x + ',' + d.y + ')';
+    });
 
-  graph.selectAll('g.node')
-    .attr('transform', function(d) { return 'translate(' + d.x  + ','  + d.y + ')'; })
+  if (updateColor) {
+    nodes
+      .select('circle')
+      .style('fill', getColorOfPosition)
+  }
 }
+
 
 function collide(node) {
   var r = (node.radius || radius) + 16,
@@ -41,7 +48,7 @@ function collide(node) {
     nx2 = node.x + r,
     ny1 = node.y - r,
     ny2 = node.y + r;
-  return function(quad, x1, y1, x2, y2) {
+  return function (quad, x1, y1, x2, y2) {
     if (quad.point && (quad.point !== node)) {
       var x = node.x - quad.point.x,
         y = node.y - quad.point.y,
@@ -59,7 +66,7 @@ function collide(node) {
   };
 }
 
-function addNodes (nodes, params) {
+function addNodes(nodes, params) {
   var node = nodes.enter()
     .append('g')
     .attr('class', 'node');
@@ -67,7 +74,7 @@ function addNodes (nodes, params) {
   node
     .append('circle')
     .attr('r', radius)
-    .style('fill', params.color);
+    .style('fill', params.fill || '#000')
 
   node
     .append('text')
@@ -79,7 +86,7 @@ function addNodes (nodes, params) {
 }
 
 
-function selectNodes (d) {
+function selectNodes(d) {
   var nodes = graph.selectAll('g.node').data(d, function (node) {
     return node.id;
   });
@@ -91,21 +98,32 @@ function selectNodes (d) {
   return nodes;
 }
 
-function displayDomains (domains) {
+function displayDomains(domains) {
   var nodes = selectNodes(domains);
 
-  addNodes(nodes, {
-    color: function(d, i) { return color(i); }
-  });
-
   force.nodes(domains).start();
+
+  addNodes(nodes, {
+    color: getColorOfPosition
+  });
 }
 
-function displayTypes (color, types) {
+function getColorOfPosition(point) {
+  var distanceX = point.x - width / 2;
+  var distanceY = point.y - height / 2;
+  var distance = Math.sqrt((distanceX * distanceX + distanceY * distanceY));
+  var lightness = 100 - Math.round(Math.min(Math.max((distance / (height / 2)) * 40 + 10, 0), 100));
+  var hue = Math.round((Math.atan2(distanceX, distanceY) / Math.PI) * 180);
+
+  return 'hsl(' + hue + ', 75%, ' + lightness + '%)'
+}
+
+
+function displayTypes(color, types) {
   var nodes = selectNodes(types);
 
   addNodes(nodes, {
-    color: color
+    fill: color
   });
 
   force.nodes(types).start();
@@ -115,8 +133,10 @@ freebase.getDomains().then(function (domains) {
 
   displayDomains(domains);
 
-  graph.selectAll('.node').on('click', function (data, i) {
-    var c = color(i);
-    freebase.getTypes(data.id).then(_.partial(displayTypes, c));
+  graph.selectAll('.node').on('click', function (entry) {
+    updateColor = false;
+
+    var color = getColorOfPosition(entry);
+    freebase.getTypes(entry.id).then(_.partial(displayTypes, color));
   });
 });
