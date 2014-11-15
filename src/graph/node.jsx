9324c36259787a0702f10f4c5b4bf3fb827110d7
Paul Sonnentag <Paul.Sonnentag@googlemail.com>
2014-11-16 00:53:56 +0100

    Adds text rect

--- src/graph/node.jsx
+++ src/graph/node.jsx
@@ -1,13 +1,14 @@
 'use strict';
 
 var React = require('react');
+var TextRect = require('./text-rect.jsx');
 
 module.exports = React.createClass({
   render: function () {
    return <g key={this.props.key} className="node"
              transform={'translate('+ this.props.x + ',' + this.props.y + ')'}>
      <circle r={this.props.radius} fill="red" />
-     <text>{this.props.name}</text>
+     <TextRect maxLength={10} lineHeight="1.2em">{this.props.name}</TextRect>
    </g>
   }
 });
